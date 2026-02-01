6f218d62d8373cee05825b739d56debd60b6d2a0
Krishna <krishna@Krishnas-MacBook-Air.local>
2026-02-01 23:32:08 +0530

    done

--- app/main.py
+++ app/main.py
@@ -87,10 +87,6 @@ app.add_middleware(
         "https://admin.closeuplovetunes.in",
         "https://closeuplovetunes.in",
         "https://www.closeuplovetunes.in"
-        "http://127.0.0.1:3001",
-        "http://127.0.0.1:3000",
-        "http://localhost:3001",
-        "http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
